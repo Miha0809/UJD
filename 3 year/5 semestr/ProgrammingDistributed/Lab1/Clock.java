8398ab76792f6f89252206def28d0a955646ce15
Miha0809 <miha080104@gmail.com>
2024-10-15 14:39:47 +0200

    Task3

--- 3 year/5 semestr/ProgrammingDistributed/Lab1/Clock.java
+++ 3 year/5 semestr/ProgrammingDistributed/Lab1/Clock.java
@@ -1,32 +1,59 @@
-import java.text.SimpleDateFormat;
-import java.time.LocalTime;
-import java.util.Locale;
-
 public class Clock {
-  private LocalTime localTime;
-  private boolean isFormat24h;
+  private int hours = 0;
+  private int minutes = 0;
+  private int seconds = 0;
+  private String period;
+  private boolean isFormat24h = true;
 
-  public Clock() {
-    localTime = LocalTime.of(0, 0);
-    isFormat24h = true;
+  public Clock() {}
+
+  public void setTime(int hours, int minutes, int seconds) {
+    this.hours = hours % 24;
+    this.minutes = minutes % 60;
+    this.seconds = seconds % 60;
   }
 
-  public Clock(LocalTime localTime, boolean isFormat24h) {
-    this.localTime = localTime;
+  public String display() {
+    String result = "";
+
+    if (isFormat24h) {
+      result = hours + ":" + minutes + ":" + seconds;
+    } else if (!isFormat24h) {
+      period = hours < 12 ? "AM" : "PM";
+      result = (hours % 12) + ":" + minutes + ":" + seconds + " " + period;
+    }
+
+    return result;
+  }
+
+  public void format(boolean isFormat24h) {
     this.isFormat24h = isFormat24h;
   }
 
-  public void setTime(int hours, int minutes, int seconds) {
-    localTime = LocalTime.of(hours, minutes, seconds);
+  public void tick() {
+    seconds++;
+
+    if (seconds >= 60) {
+      seconds -= 60;
+      minutes++;
+
+      if (minutes >= 60) {
+        minutes -= 60;
+        hours++;
+      }
+    }
   }
 
-  public void display() {
-    if (isFormat24h) {
-      SimpleDateFormat simpleDateFormatArrivals = new SimpleDateFormat("HH:mm", Locale.UK);
-      System.out.println(simpleDateFormatArrivals.toString());
-    } else {
-      SimpleDateFormat simpleDateFormatArrivals = new SimpleDateFormat("hh:mm", Locale.UK);
-      System.out.println(simpleDateFormatArrivals.toString());
+  public void start5sec() {
+    try {
+      long count = 0;
+      while (count != 5) {
+        count++;
+        tick();
+        Thread.sleep(1000);
+      }
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
     }
   }
 }
--- 3 year/5 semestr/ProgrammingDistributed/Lab1/Main.java
+++ 3 year/5 semestr/ProgrammingDistributed/Lab1/Main.java
@@ -10,6 +10,7 @@ public class Main {
       System.out.println("Enter 1 - task 1");
       System.out.println("Enter 2 - task 2");
       System.out.println("Enter 3 - task 3");
+      System.out.println("Enter 4 - task 4");
       mode = scanner.nextInt();
 
       if (mode == 1) {
@@ -18,6 +19,8 @@ public class Main {
         task2();
       } else if (mode == 3) {
         task3();
+      } else if (mode == 4) {
+        task4();
       }
     }
 
@@ -42,5 +45,35 @@ public class Main {
     Clock clock = new Clock();
     clock.setTime(12, 25, 40);
     clock.display();
+  }
+
+  private static void task4() {
+    Clock clock1 = new Clock();
+    clock1.format(false);
+    clock1.setTime(22, 2, 2);
+    System.out.println(clock1.display());
+    clock1.tick();
+    System.out.println(clock1.display());
+    clock1.tick();
+    System.out.println(clock1.display());
+    clock1.tick();
+    System.out.println(clock1.display());
+    clock1.start5sec();
+    System.out.println(clock1.display());
+
+    System.out.println();
+
+    Clock clock2 = new Clock();
+    clock2.format(true);
+    clock2.setTime(22, 2, 2);
+    System.out.println(clock2.display());
+    clock2.tick();
+    System.out.println(clock2.display());
+    clock2.tick();
+    System.out.println(clock2.display());
+    clock2.tick();
+    System.out.println(clock2.display());
+    clock2.start5sec();
+    System.out.println(clock2.display());
   }
 }
