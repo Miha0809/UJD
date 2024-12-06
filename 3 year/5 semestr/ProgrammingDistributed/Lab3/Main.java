a51f59d7ab8207f52828c9acf55c8338ce2835a1
Miha0809 <miha080104@gmail.com>
2024-12-07 00:27:18 +0100

    Task1

--- 3 year/5 semestr/ProgrammingDistributed/Lab3/Main.java
+++ 3 year/5 semestr/ProgrammingDistributed/Lab3/Main.java
@@ -1,6 +1,9 @@
-import Task2.Buffer;
-import Task2.Consumer;
-import Task2.Producer;
+import Task1.Buffer;
+import Task1.Consumer;
+import Task1.Producer;
+// import Task2.Buffer;
+// import Task2.Consumer;
+// import Task2.Producer;
 
 public class Main {
   public static void main(String[] args) {
@@ -10,8 +13,21 @@ public class Main {
     Buffer buffer = new Buffer(lengthBuffer);
     Thread producent = new Thread(new Producer(buffer, lengthProducer));
     Thread consumer = new Thread(new Consumer(buffer));
+    Thread consumer2 = new Thread(new Consumer(buffer));
 
     producent.start();
     consumer.start();
+    consumer2.start();
+
+    // 2 Task
+    // final int lengthBuffer = 10;
+    // final int lengthProducer = 50;
+    
+    // Buffer buffer = new Buffer(lengthBuffer);
+    // Thread producent = new Thread(new Producer(buffer, lengthProducer));
+    // Thread consumer = new Thread(new Consumer(buffer));
+
+    // producent.start();
+    // consumer.start();
   }
 }
