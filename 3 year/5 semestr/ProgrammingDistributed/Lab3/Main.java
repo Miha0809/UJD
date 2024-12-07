d39c4c4be66b9201bb43f883f7782d98827f0d5c
Miha0809 <miha080104@gmail.com>
2024-12-07 11:14:49 +0100

    Task3

--- 3 year/5 semestr/ProgrammingDistributed/Lab3/Main.java
+++ 3 year/5 semestr/ProgrammingDistributed/Lab3/Main.java
@@ -1,25 +1,23 @@
-import Task1.Buffer;
-import Task1.Consumer;
-import Task1.Producer;
-// import Task2.Buffer;
-// import Task2.Consumer;
-// import Task2.Producer;
+// import Task1.*;
+// import Task2.*;
+import Task3.*;
 
 public class Main {
   public static void main(String[] args) {
-    final int lengthBuffer = 10;
-    final int lengthProducer = 50;
+    // Task 1
+    // final int lengthBuffer = 10;
+    // final int lengthProducer = 50;
     
-    Buffer buffer = new Buffer(lengthBuffer);
-    Thread producent = new Thread(new Producer(buffer, lengthProducer));
-    Thread consumer = new Thread(new Consumer(buffer));
-    Thread consumer2 = new Thread(new Consumer(buffer));
+    // Buffer buffer = new Buffer(lengthBuffer);
+    // Thread producent = new Thread(new Producer(buffer, lengthProducer));
+    // Thread consumer = new Thread(new Consumer(buffer));
+    // Thread consumer2 = new Thread(new Consumer(buffer));
 
-    producent.start();
-    consumer.start();
-    consumer2.start();
+    // producent.start();
+    // consumer.start();
+    // consumer2.start();
 
-    // 2 Task
+    // Task 2
     // final int lengthBuffer = 10;
     // final int lengthProducer = 50;
     
@@ -29,5 +27,16 @@ public class Main {
 
     // producent.start();
     // consumer.start();
+
+    int maxReads = 10;
+    Buffer buffer = new Buffer();
+
+    Thread sensorA = new Thread(new Sensor(buffer, true, maxReads));
+    Thread sensorB = new Thread(new Sensor(buffer, false, maxReads));
+    Thread calculator = new Thread(new Calculator(buffer, maxReads));
+
+    sensorA.start();
+    sensorB.start();
+    calculator.start();
   }
 }
