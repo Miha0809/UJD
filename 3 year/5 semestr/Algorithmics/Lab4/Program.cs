4286207df3c92742056c0b5ccb7403bedc909e97
Miha0809 <miha080104@gmail.com>
2024-12-14 16:24:57 +0100

    Task1

---
+++ 3 year/5 semestr/Algorithmics/Lab4/Program.cs
@@ -0,0 +1,23 @@
+﻿var edges = new (int, int)[]
+{
+    (0, 1),
+    (1, 2),
+    (3, 4),
+    (5, 6),
+    (7, 8)
+};
+
+int size = edges.Length * 2;
+var dsu = new DisjointSetUnion(size);
+
+foreach (var edge in edges)
+{
+    dsu.Union(edge.Item1, edge.Item2);
+}
+
+Console.WriteLine(dsu.ToString());
+
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Element {i} belongs {dsu.Find(i)}");
+}
