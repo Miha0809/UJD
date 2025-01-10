3dacb400f419f337fa7d10fecb3ba2cdbe9bfbcb
Miha0809 <miha080104@gmail.com>
2025-01-11 00:48:45 +0100

    Task2

--- 3 year/5 semestr/ProgrammingDistributed/Lab4/Main.java
+++ 3 year/5 semestr/ProgrammingDistributed/Lab4/Main.java
@@ -1,15 +1,25 @@
-import Task1.Car;
-import Task1.Ferry;
-import Task1.FerryRunnable;
+import Task1.*;
+import Task2.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Ferry ferry = new Ferry();
-        Thread ferryThread = new Thread(new FerryRunnable(ferry));
-        ferryThread.start();
+    private static final int MAX_CAPACITY = 3;
+    private static final int NUM_ELEVATORS = 3;
+    private static final int NUM_PEOPLE = 10;
 
-        for (int i = 1; i <= 10; i++) {
-            new Thread(new Car(i, ferry)).start();
+    public static void main(String[] args) {
+        // Task 1
+        // Ferry ferry = new Ferry();
+        // Thread ferryThread = new Thread(new FerryRunnable(ferry));
+        // ferryThread.start();
+
+        // for (int i = 1; i <= 10; i++) {
+        //     new Thread(new Car(i, ferry)).start();
+        // }
+
+        // Task 2
+        Building building = new Building(NUM_ELEVATORS, MAX_CAPACITY);
+        for (int i = 1; i <= NUM_PEOPLE; i++) {
+            new Thread(new Person(i, building)).start();
         }
     }
 }
